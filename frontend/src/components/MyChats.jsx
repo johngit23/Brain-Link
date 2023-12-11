d85919fa65f02568dd9dc1b0670f2a17d4ec70d6
johngit23 <126653377+johngit23@users.noreply.github.com>
2023-12-11 22:27:55 +0300

    Update MyChats.jsx

--- frontend/src/components/MyChats.jsx
+++ frontend/src/components/MyChats.jsx
@@ -25,7 +25,7 @@ const MyChats = ({ fetchAgain }) => {
       };
 
       const { data } = await axios.get(
-        "http://localhost:3000/api/chat",
+        "brain-link.vercel.app/api/chat",
         config
       );
       console.log(data);

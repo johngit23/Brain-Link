ce8a034d0bccb0a67ead685c935c42b6cb8c631b
johngit23 <126653377+johngit23@users.noreply.github.com>
2023-12-11 22:32:41 +0300

    Update Login.jsx

--- frontend/src/components/Authentication/Login.jsx
+++ frontend/src/components/Authentication/Login.jsx
@@ -41,7 +41,7 @@ const Login = () => {
       };
 
       const { data } = await axios.post(
-        "http://localhost:3000/api/user/login",
+        "brain-link.vercel.app/api/user/login",
         { email, password },
         config
       );

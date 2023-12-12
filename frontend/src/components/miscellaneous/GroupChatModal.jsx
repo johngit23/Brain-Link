6aaa1434ddad5fd616f954733f73767388e71b07
johngit23 <126653377+johngit23@users.noreply.github.com>
2023-12-12 11:29:04 +0300

    Update GroupChatModal.jsx

--- frontend/src/components/miscellaneous/GroupChatModal.jsx
+++ frontend/src/components/miscellaneous/GroupChatModal.jsx
@@ -59,7 +59,7 @@ const GroupChatModal = ({ children }) => {
         },
       };
       const { data } = await axios.get(
-        `brain-link.vercel.app/api/user?search=${search}`,
+        `http://localhost:3000/api/user?search=${search}`,
         config
       );
       console.log(data);
@@ -100,7 +100,7 @@ const GroupChatModal = ({ children }) => {
         },
       };
       const { data } = await axios.post(
-        `brain-link.vercel.app/api/chat/group`,
+        `http://localhost:3000/api/chat/group`,
         {
           name: groupChatName,
           users: JSON.stringify(selectedUsers.map((u) => u._id)),

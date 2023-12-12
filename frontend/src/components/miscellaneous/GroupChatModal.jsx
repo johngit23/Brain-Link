c50337790c596f10bcf68d68fc2a9676585d0178
johngit23 <126653377+johngit23@users.noreply.github.com>
2023-12-12 12:20:23 +0300

    Update GroupChatModal.jsx

--- frontend/src/components/miscellaneous/GroupChatModal.jsx
+++ frontend/src/components/miscellaneous/GroupChatModal.jsx
@@ -59,7 +59,7 @@ const GroupChatModal = ({ children }) => {
         },
       };
       const { data } = await axios.get(
-        `http://localhost:3000/api/user?search=${search}`,
+        `https://brain-link.onrender.com/api/user?search=${search}`,
         config
       );
       console.log(data);
@@ -100,7 +100,7 @@ const GroupChatModal = ({ children }) => {
         },
       };
       const { data } = await axios.post(
-        `http://localhost:3000/api/chat/group`,
+        `https://brain-link.onrender.com/api/chat/group`,
         {
           name: groupChatName,
           users: JSON.stringify(selectedUsers.map((u) => u._id)),

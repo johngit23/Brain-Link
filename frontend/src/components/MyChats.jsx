2c0fc77d5e0c9584ce66c90a5ff98877d1511c7b
johngit23 <126653377+johngit23@users.noreply.github.com>
2023-12-12 13:18:02 +0300

    Update MyChats.jsx

--- frontend/src/components/MyChats.jsx
+++ frontend/src/components/MyChats.jsx
@@ -25,7 +25,7 @@ const MyChats = ({ fetchAgain }) => {
       };
 
       const { data } = await axios.get(
-        "http://localhost:3000/api/chat",
+        "https://brain-link.onrender.com/api/chat",
         config
       );
       console.log(data);

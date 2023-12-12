dfea18bb9739a6bdffb28b6ab6df75763ca09092
johngit23 <126653377+johngit23@users.noreply.github.com>
2023-12-12 11:31:14 +0300

    Update UpdateGroupChatModal.jsx

--- frontend/src/components/miscellaneous/UpdateGroupChatModal.jsx
+++ frontend/src/components/miscellaneous/UpdateGroupChatModal.jsx
@@ -47,7 +47,7 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
         },
       };
       const { data } = await axios.get(
-        `brain-link.vercel.app/api/user?search=${search}`,
+        `http://localhost:3000/api/user?search=${search}`,
         config
       );
       console.log(data);
@@ -77,7 +77,7 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
         },
       };
       const { data } = await axios.put(
-        `brain-link.vercel.app/api/chat/rename`,
+        `http://localhost:3000/api/chat/rename`,
         {
           chatId: selectedChat._id,
           chatName: groupChatName,
@@ -135,7 +135,7 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
         },
       };
       const { data } = await axios.put(
-        `brain-link.vercel.app/api/chat/groupadd`,
+        `http://localhost:3000/api/chat/groupadd`,
         {
           chatId: selectedChat._id,
           userId: user1._id,
@@ -180,7 +180,7 @@ const UpdateGroupChatModal = ({ fetchMessages, fetchAgain, setFetchAgain }) => {
         },
       };
       const { data } = await axios.put(
-        `brain-link.vercel.app/api/chat/groupremove`,
+        `http://localhost:3000/api/chat/groupremove`,
         {
           chatId: selectedChat._id,
           userId: user1._id,
